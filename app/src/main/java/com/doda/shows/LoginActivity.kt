a9d425921d32746f1a85be649edb2d145a7ca9f2
Doda94 <56204086+Doda94@users.noreply.github.com>
2022-07-11 14:00:39 +0200

    make login button to launch a new activity

--- app/src/main/java/com/doda/shows/LoginActivity.kt
+++ app/src/main/java/com/doda/shows/LoginActivity.kt
@@ -1,11 +1,40 @@
 package com.doda.shows
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.doda.shows.databinding.ActivityLoginBinding
+import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        // needs a rewrite with better button behaviour
+        binding.loginButton.setOnClickListener(){
+            var email = binding.emailEditText.text.toString()
+            var password = binding.passwordEditText.text.toString()
+            if (TextUtils.isEmpty(email)){
+                binding.emailEditText.setError(getString(R.string.email_error))
+            }
+            if (password.length < 6){
+                binding.passwordEditText.setError(getString(R.string.password_error))
+            }
+            if (!TextUtils.isEmpty(email) && password.length>=6){
+                val intent = Intent (this,WelcomeActivitiy::class.java)
+                startActivity(intent)
+            }
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/doda/shows/WelcomeActivitiy.kt
@@ -0,0 +1,11 @@
+package com.doda.shows
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivitiy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
