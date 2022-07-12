ed11d8b8296ac006d9af0232ccc374e7e927fb27
Doda94 <56204086+Doda94@users.noreply.github.com>
2022-07-13 00:33:50 +0200

    fix login button behavior, finish welcome screen

--- app/src/main/java/com/doda/shows/LoginActivity.kt
+++ app/src/main/java/com/doda/shows/LoginActivity.kt
@@ -2,15 +2,42 @@ package com.doda.shows
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
-import android.widget.EditText
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import com.doda.shows.databinding.ActivityLoginBinding
-import org.w3c.dom.Text
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityLoginBinding
+
+    val EMAIL_ADDRESS_PATTERN = Pattern.compile(
+        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+    )
+
+    fun isEmailValid(str: String): Boolean {
+        return EMAIL_ADDRESS_PATTERN.matcher(str).matches()
+    }
+
+    var email_valid = false
+    var password_valid = false
+
+    fun disableButton(button: Button) {
+        binding.loginButton.isEnabled = false
+        binding.loginButton.alpha = 0.5F
+    }
+
+    fun enableButton(button: Button) {
+        binding.loginButton.isEnabled = true
+        binding.loginButton.alpha = 1F
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,21 +45,30 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        disableButton(binding.loginButton)
 
-        // needs a rewrite with better button behaviour
-        binding.loginButton.setOnClickListener(){
-            var email = binding.emailEditText.text.toString()
-            var password = binding.passwordEditText.text.toString()
-            if (TextUtils.isEmpty(email)){
-                binding.emailEditText.setError(getString(R.string.email_error))
+        binding.emailEditText.doOnTextChanged { text, start, before, count ->
+            email_valid = isEmailValid(binding.emailEditText.text.toString())
+            if (email_valid && password_valid) {
+                enableButton(binding.loginButton)
+            } else {
+                disableButton(binding.loginButton)
             }
-            if (password.length < 6){
-                binding.passwordEditText.setError(getString(R.string.password_error))
+        }
+
+        binding.passwordEditText.doOnTextChanged { text, start, before, count ->
+            password_valid = binding.passwordEditText.text.toString().length >= 6
+            if (email_valid && password_valid) {
+                enableButton(binding.loginButton)
+            } else {
+                disableButton(binding.loginButton)
             }
-            if (!TextUtils.isEmpty(email) && password.length>=6){
-                val intent = Intent (this,WelcomeActivitiy::class.java)
-                startActivity(intent)
-            }
+        }
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, WelcomeActivitiy::class.java)
+            intent.putExtra("username", binding.emailEditText.text.toString().substringBefore("@"))
+            startActivity(intent)
         }
 
 
--- app/src/main/java/com/doda/shows/WelcomeActivitiy.kt
+++ app/src/main/java/com/doda/shows/WelcomeActivitiy.kt
@@ -2,10 +2,24 @@ package com.doda.shows
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.doda.shows.databinding.ActivityWelcomeBinding
 
 class WelcomeActivitiy : AppCompatActivity() {
+
+    lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val username = intent.getStringExtra("username")
+        val welcomeMessage = R.string.welcome.toString() + ", " + username + "!"
+
+        binding.welcomeText.setText(welcomeMessage)
+
+
     }
 }
