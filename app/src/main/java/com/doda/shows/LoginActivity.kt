af4294dfe33212555be68e1ef449bcfae2b573b6
Doda94 <56204086+Doda94@users.noreply.github.com>
2022-07-19 14:05:40 +0200

    make rating bar mandatory, change button color

--- app/src/main/java/com/doda/shows/LoginActivity.kt
+++ app/src/main/java/com/doda/shows/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.doda.shows
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import com.doda.shows.databinding.ActivityLoginBinding
--- app/src/main/java/com/doda/shows/ShowDetailsActivity.kt
+++ app/src/main/java/com/doda/shows/ShowDetailsActivity.kt
@@ -1,6 +1,7 @@
 package com.doda.shows
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -13,14 +14,14 @@ class ShowDetailsActivity : AppCompatActivity() {
 
     private lateinit var reviews: List<Review>
 
-    private lateinit var binding : ActivityShowDetailsBinding
+    private lateinit var binding: ActivityShowDetailsBinding
 
     private lateinit var adapter: ReviewsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding= ActivityShowDetailsBinding.inflate(layoutInflater)
+        binding = ActivityShowDetailsBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
 
@@ -37,7 +38,7 @@ class ShowDetailsActivity : AppCompatActivity() {
 
     }
 
-    private fun initReviewsRecycler(){
+    private fun initReviewsRecycler() {
         adapter = ReviewsAdapter(reviews)
 
         binding.reviewsRecyclerView.layoutManager = LinearLayoutManager(this)
@@ -49,7 +50,7 @@ class ShowDetailsActivity : AppCompatActivity() {
         )
     }
 
-    private fun addReviewBottomSheet(){
+    private fun addReviewBottomSheet() {
         val dialog = BottomSheetDialog(this)
 
         val bottomSheetBinding = AddReviewBottomSheetBinding.inflate(layoutInflater)
@@ -58,44 +59,47 @@ class ShowDetailsActivity : AppCompatActivity() {
 
         dialog.show()
 
-        bottomSheetBinding.closeBottomSheetButton.setOnClickListener{
+        bottomSheetBinding.closeBottomSheetButton.setOnClickListener {
             dialog.dismiss()
         }
 
         bottomSheetBinding.writeRatingBar.rating = adapter.average
 
-        bottomSheetBinding.submitReviewButton.setOnClickListener{
+        bottomSheetBinding.submitReviewButton.setOnClickListener {
             val name: String = intent.getStringExtra("username").toString()
             var desc: String = bottomSheetBinding.reviewTextInputEditText.text.toString()
             if (desc.isEmpty()) {
                 desc = ""
             }
             val rating: Int = bottomSheetBinding.writeRatingBar.rating.toInt()
+            if (rating == 0) {
+                Toast.makeText(this, getString(R.string.rating_error), Toast.LENGTH_SHORT).show()
+            } else {
+                adapter.addReview(Review(name, rating, desc))
+                updateRatingBar()
+                showReviews()
 
-            adapter.addReview(Review(name, rating, desc))
-            updateRatingBar()
-            showReviews()
-
-            dialog.dismiss()
+                dialog.dismiss()
+            }
         }
     }
 
-    private fun showReviews(){
-        if (adapter.itemCount > 0){
+    private fun showReviews() {
+        if (adapter.itemCount > 0) {
             binding.noReviewsConstraintLayout.isVisible = false
             binding.reviewsConstraintLayout.isVisible = true
         }
     }
 
-    private fun updateRatingBar(){
+    private fun updateRatingBar() {
         binding.reviewsText.text = getString(R.string.rating_bar_text, adapter.itemCount, adapter.average)
         binding.ratingBar.rating = adapter.average
     }
 
-    private fun addShowInfo(){
+    private fun addShowInfo() {
         binding.toolbarLayout.title = intent.getStringExtra("show_name")
         // TODO: add blank img
-        val imageResourceId: Int = intent.getIntExtra("show_img_id",R.drawable.ic_office)
+        val imageResourceId: Int = intent.getIntExtra("show_img_id", R.drawable.ic_office)
         binding.showMenuImage.setImageResource(imageResourceId)
         binding.showMenuDescription.text = intent.getStringExtra("show_desc")
     }
