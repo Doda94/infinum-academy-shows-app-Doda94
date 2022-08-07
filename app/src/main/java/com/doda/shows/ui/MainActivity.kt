de89777c2d68c6db04cbe2200683eafa88f7cd74
Doda94 <56204086+Doda94@users.noreply.github.com>
2022-08-07 23:37:10 +0200

    add splash screen

--- app/src/main/java/com/doda/shows/ui/MainActivity.kt
+++ app/src/main/java/com/doda/shows/ui/MainActivity.kt
@@ -3,12 +3,16 @@ package com.doda.shows.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.doda.shows.databinding.ActivityMainBinding
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
